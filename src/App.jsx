585f9b610eb3231d68d75fbdfbfbf82b36ea22c2
lindath0905 <linda.thurner@outlook.at>
2026-03-16 18:56:49 +0100

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ export default function App() {
     <div className="App">
       <h1>Weather search app</h1>
       <WeatherSearch />
+      <footer>
+        <a href="https://github.com/lindath0905/weather-react">
+          Open-source code
+        </a>
+        , by <a href="https://github.com/lindath0905">Linda Thurner</a>
+      </footer>
     </div>
   );
 }
